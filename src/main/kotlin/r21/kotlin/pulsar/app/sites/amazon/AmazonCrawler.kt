6d57a831731f6c0c6c8c5bc062280356e6315da9
aspirina765 <molinalucas765@gmail.com>
2023-03-01 04:02:34 -0300

    initial commit

--- src/main/kotlin/r21/kotlin/pulsar/app/sites/amazon/AmazonCrawler.kt
+++ src/main/kotlin/r21/kotlin/pulsar/app/sites/amazon/AmazonCrawler.kt
@@ -4,6 +4,6 @@ import ai.platon.pulsar.context.PulsarContexts
 
 fun main() = PulsarContexts.createSession().scrapeOutPages(
     "https://www.amazon.com/Best-Sellers/zgbs",
-    "-outLink a[href~=/dp/] -headless",
+    "-headless -outLink a[href~=/dp/]",
     mapOf("title" to "#title", "ratings" to "#acrCustomerReviewText")
 ).let { println(it) }
